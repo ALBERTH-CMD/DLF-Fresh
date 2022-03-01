741f71627bd94470c198d3c14caed0ffb1501c80
ALBERTH-CMD <GomezAlbertOlivares@gmail.com>
2022-02-28 18:13:38 -0600

    V2

---
+++ resources/views/tarjects.blade.php
@@ -0,0 +1,41 @@
+<main class="p-10 rounded-lg shadow-2xl bg-white max-w-full">
+
+    <div class="container-box">
+
+        <div class="box box1">
+            <img src="{{ asset('img/img1.svg') }}" alt="">
+            <h2>{{$config->frase_1}}</h2>
+
+            <div class="container-p"><p>{{$config->frase_1}}</p></div>
+
+            <div class="check">
+                <i class="fas fa-check"></i>
+            </div>
+        </div>
+
+        <div class="box box2">
+            <img src="{{ asset('img/img2.svg') }}" alt="">
+            <h2>{{$config->frase_2}}</h2>
+            <div class="container-p">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos minus non voluptatibus rem harum iste vero magnam voluptate, id laboriosam?...</p>
+            </div>
+
+            <div class="check">
+                <i class="fas fa-check"></i>
+            </div>
+        </div>
+
+        <div class="box box3">
+            <img src="{{ asset('img/img3.svg') }}" alt="">
+            <h2>{{$config->frase_3}}</h2>
+            <div class="container-p">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.. Dignissimos minus non voluptatibus rem harum iste vero magnam voluptate, id laboriosam?...</p>
+            </div>
+
+            <div class="check">
+                <i class="fas fa-check"></i>
+            </div>
+        </div>
+
+    </div>
+</main>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,90 @@
+<x-app-layout>
+
+    <div class="p-0 container-fluid">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                @forelse ($carrusel as $item)
+                <li data-target="#carouselExampleCaptions" data-slide-to="{{$item->orden}}" class="@if($loop->index==0) active @endif"></li>
+                @empty
+                @endforelse
+            </ol>
+            <div class="carousel-inner">
+            @forelse ($carrusel as $item)
+              <div class="carousel-item @if($loop->index==0) active @endif">
+                <img src="{{ asset('/img/carrusel') }}/{{$item->urlfoto}}" class="d-block w-100" alt="{{$item->frase}}">
+                <div class="pb-5 carousel-caption d-none d-md-block">
+                    <h2>{{$item->descripcion}}</h2>
+                    <a href="{{$item->link}}" target="_Blank" class="btn btn-outline-primary">VER MÁS</a>
+                </div>
+              </div>
+            @empty
+            @endforelse
+
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+        <div>
+        <h3 class="p-10 rounded-lg shadow-2xl bg-white max-w-full" style="font-family: fangsong;  font-size: 25px;">{{$config->slogan}}</h3>
+            {{-- Tarjets --}}
+                @include('tarjects')
+        </div>
+
+    </div>
+
+    <!-- Productos Para Exportar -->
+
+    <div>
+        <div class="p-10 rounded-lg shadow-2xl bg-white max-w-full">
+          <div class="p-10 rounded-lg shadow-2xl bg-blue-200 max-w-full">
+            <h1 class="flex justify-center text-lg font-medium leading-10 text-gray-900" style="font-family: fangsong;  font-size: 25px;">CONTACTO</h1>
+            <p style="font-family: fangsong;  font-size: 25px; text-align:center;"> {{$config->celular}}</p>
+          </div>
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold text-center">Exportaciones ACTUALES Y A FUTURO</div>
+                @forelse ($producto as $item)
+                    <div class="col-sm-4">
+                        <div class="card">
+                            <a href="artesanias/{{$item->categoria->slug}}/{{$item->slug}}">
+                                <img src="{{ asset('/img/producto') }}/{{$item->urlfoto}}" class="card-img-top" alt="{{$item->nombre}}">
+                            </a>
+                        </div>
+                        <div class="card-footer">
+                            <a href="artesanias/{{$item->categoria->slug}}/{{$item->slug}}" class="btn btn-outline-warning btn-block">{{$item->nombre}}</a>
+                        </div>
+                    </div>
+                    @empty
+                @endforelse
+
+            </div>
+        </div>
+    </div>
+    <!-- Productos para Exportar /-->
+
+
+<!---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------/-->
+    <!-- publicaciones-->
+    <div class="max-w-md mx-auto mt-5 overflow-hidden bg-white shadow-2xl rounded-xl md:max-w-2xl">
+        @foreach ($posts as $r)
+
+            <div class="md:flex">
+                <div class="md:shrink-0">
+                <img class="object-cover w-full h-85 md:h-full md:w-150" src="{{ asset('/img/post') }}/{{$r->urlfoto}}" alt="{{$r->nombre}}">
+                </div>
+                <div class="p-8">
+                <div class="text-sm font-semibold tracking-wide text-indigo-500 uppercase">{{$r->nombre}}</div>
+                <p class="mt-2 text-gray-500">{{$r->description}}</p>
+                <span class="small">{{$r->created_at->diffForHumans()}}</span>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <!-- publicaciones/-->
+</x-app-layout>
